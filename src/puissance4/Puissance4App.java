44d2a2d19a2937ed63f3b2264df8f27552de1746
Ténénan <tenenan.thebest@gmail.com>
2023-12-16 00:17:01 +0100

    Comfflit de version (java et javaFX) à resoudre la prochaine fois

--- src/puissance4/Puissance4App.java
+++ src/puissance4/Puissance4App.java
@@ -44,7 +44,7 @@ public class Puissance4App extends Application {
 
     private Button createGridButton(int row, int col) {
         Button button = new Button();
-        // Configurez le style et la taille des boutons selon vos besoins
+        // Configurer le style et la taille des boutons selon vos besoins
         button.setMinSize(50, 50);
         button.setOnAction(e -> handleButtonClick(row, col));
         return button;
@@ -52,6 +52,40 @@ public class Puissance4App extends Application {
 
     private void handleButtonClick(int row, int col) {
         System.out.println("Button clicked at row: " + row + ", col: " + col);
-        // Ajoutez ici la logique pour gérer le clic sur un bouton de la grille
+
+        // Ajouter ici la logique pour gérer le clic sur un bouton de la grille
+
+        // Pour les boutons
+        Button clickedButton = gridButtons[row][col]; // Supposons que gridButtons est une matrice de boutons représentant la grille
+        String currentPlayer = isRedTurn ? "Rouge" : "Jaune"; // Supposons une variable isRedTurn pour suivre le tour actuel
+
+        if (isValidMove(row, col)) {
+            // Vérifier si le mouvement est valide. Par exemple, si la colonne n'est pas pleine
+
+            // Mettre à jour le bouton avec la couleur du joueur actuel
+            clickedButton.setStyle("-fx-background-color: " + (isRedTurn ? "red" : "yellow"));
+
+            // Mettre à jour la matrice de jeu. Par exemple, avec une valeur pour représenter le joueur actuel
+            gameBoard[row][col] = isRedTurn ? "R" : "J";
+
+            // Vérifiez s'il y a un gagnant
+            if (checkForWinner(row, col)) {
+                System.out.println(currentPlayer + " a gagné !");
+                // Ajouter ici la logique pour traiter la fin du jeu
+            } else {
+                // Passer au tour suivant
+                isRedTurn = !isRedTurn;
+            }
+        } else {
+            System.out.println("Mouvement invalide. La colonne est pleine.");
+            /* Ajouter ici la logique pour traiter un mouvement invalide.
+            Par exemple, affichez un message à l'utilisateur ou désactivez le bouton */
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Mouvement invalide");
+            alert.setHeaderText(null);
+            alert.setContentText("La colonne est pleine. Choisissez une autre colonne.");
+            alert.showAndWait();
+        }
     }
+
 }
